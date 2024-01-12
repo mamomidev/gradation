4335290df80a18a9291a6159a8a4d46eed8963db
heroq <96118954+heroq@users.noreply.github.com>
2024-01-12 17:52:19 +0900

    Init

---
+++ src/main/java/org/hh99/gradation/GradationApplication.java
@@ -0,0 +1,13 @@
+package org.hh99.gradation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hh99/gradation/GradationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hh99.gradation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
