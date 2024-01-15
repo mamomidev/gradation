5f6f6f05f411c95adef818029a936b15fe3bfadc
mum <mumum3521@naver.com>
2024-01-16 03:02:21 +0900

    Feat : health 컨트롤러 추가

---
+++ src/main/java/org/hh99/gradation/controller/HealthController.java
@@ -0,0 +1,14 @@
+package org.hh99.gradation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String check() {
+        return "ok";
+    }
+}
