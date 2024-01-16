9396790f33d3cfe3e2a5c314e92039d0195efe57
heroq <96118954+heroq@users.noreply.github.com>
2024-01-16 14:00:42 +0900

    Refactor: 오류, 성공 메세지 정리

--- src/main/java/org/hh99/gradation/message/SuccessMessage.java
+++ src/main/java/org/hh99/gradation/message/SuccessMessage.java
@@ -2,10 +2,7 @@ package org.hh99.gradation.message;
 
 public enum SuccessMessage {
     JOIN_SUCCESS_MESSAGE("회원가입이 완료되었습니다."),
-    LOGIN_SUCCESS_MESSAGE("로그인이 완료되었습니다."),
-    ADD_CART_SUCCESS_MESSAGE("장바구니에 추가 되었습니다."),
-    UPDATE_CART_SUCCESS_MESSAGE("장바구니의 상품 개수가 수정 되었습니다."),
-    DELETE_CART_SUCCESS_MESSAGE("장바구니 상품 삭제되었습니다.");
+    LOGIN_SUCCESS_MESSAGE("로그인이 완료되었습니다.");
 
     private final String successMessage;
 
