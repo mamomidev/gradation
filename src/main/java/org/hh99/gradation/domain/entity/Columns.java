7c7937c4e834ab3e479fc4acac88191874a17eb4
mum <mumum3521@naver.com>
2024-01-16 13:50:15 +0900

    Feat : 컬럼 엔티티 생성

--- src/main/java/org/hh99/gradation/domain/entity/Columns.java
+++ src/main/java/org/hh99/gradation/domain/entity/Columns.java
@@ -1,4 +1,29 @@
 package org.hh99.gradation.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
 public class Columns {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private String columnName;
+
+    @Column
+    private Integer columnOrder;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private Board board;
 }
