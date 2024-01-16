baab2734e563361295c4200765b6ed13cfa42de7
heroq <96118954+heroq@users.noreply.github.com>
2024-01-16 14:09:52 +0900

    Feat: 회원가입 추가

--- src/main/java/org/hh99/gradation/controller/AuthController.java
+++ src/main/java/org/hh99/gradation/controller/AuthController.java
@@ -1,4 +1,24 @@
 package org.hh99.gradation.controller;
 
+import org.hh99.gradation.domain.dto.UserDto;
+import org.hh99.gradation.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class AuthController {
+
+	private final AuthService authService;
+
+	@PostMapping("/signup")
+	public ResponseEntity signup(@RequestBody UserDto userDto){
+		return authService.signup(userDto);
+	}
 }
--- src/main/java/org/hh99/gradation/service/AuthService.java
+++ src/main/java/org/hh99/gradation/service/AuthService.java
@@ -1,4 +1,30 @@
 package org.hh99.gradation.service;
 
+import org.hh99.gradation.config.PasswordConfig;
+import org.hh99.gradation.domain.dto.UserDto;
+import org.hh99.gradation.domain.entity.User;
+import org.hh99.gradation.message.ErrorMessage;
+import org.hh99.gradation.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+
+	public ResponseEntity signup(UserDto userDto) {
+		if(userRepository.findByEmail(userDto.getEmail()) != null){
+			throw new IllegalArgumentException(ErrorMessage.EMAIL_FORMAT_ERROR_MESSAGE.getErrorMessage());
+		}
+
+		userDto.setPassword(passwordEncoder.encode(userDto.getPassword()));
+		userRepository.save(new User(userDto));
+		return ResponseEntity.ok("회원가입 완료");
+	}
 }
