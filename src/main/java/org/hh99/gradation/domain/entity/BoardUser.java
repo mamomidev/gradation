7c2344da1912227ac532c56e29e65783152506eb
TaeHoon0 <kimtahwn@naver.com>
2024-01-16 18:09:02 +0900

    Feat: 보드 조회, 생성, 수정, 삭제 기능 추가

--- src/main/java/org/hh99/gradation/domain/entity/BoardUser.java
+++ src/main/java/org/hh99/gradation/domain/entity/BoardUser.java
@@ -11,7 +11,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Table
+@Table(name = "boardusers")
 @Entity
 @Getter
 @NoArgsConstructor
@@ -29,4 +29,9 @@ public class BoardUser {
 	@ManyToOne
 	@JoinColumn
 	private Board board;
+
+	public BoardUser(Board board, User user) {
+		this.board = board;
+		this.user = user;
+	}
 }
