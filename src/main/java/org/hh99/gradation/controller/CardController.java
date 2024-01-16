6862987479e2ded249f36971298215b320dbd17e
heroq <96118954+heroq@users.noreply.github.com>
2024-01-17 00:56:31 +0900

    Error: 카드 뷰 404 에러

--- src/main/java/org/hh99/gradation/controller/CardController.java
+++ src/main/java/org/hh99/gradation/controller/CardController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.hh99.gradation.domain.dto.CardDto;
 import org.hh99.gradation.service.CardService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -19,36 +21,44 @@ import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
 
-@RestController
-@RequiredArgsConstructor
+@Controller
 @RequestMapping("/api/user")
+@RequiredArgsConstructor
 public class CardController {
 
 	private final CardService cardService;
 
-	@GetMapping("/cards/{columnsId}")
-	public List<CardDto> getCards(@PathVariable Long columnsId) {
-		return cardService.getCards(columnsId);
+	@GetMapping("/cards/{cardId}")
+	public String cards(@PathVariable Long cardId, Model model){
+		model.addAttribute("test", "SI 싫어요");
+		CardDto cardDto = cardService.getCard(cardId);
+		model.addAttribute("card", cardDto);
+		return "card";
 	}
 
-	@PostMapping("/cards")
-	public ResponseEntity<String> createCard(@RequestBody CardDto cardDto, @RequestPart(value = "file", required = false) MultipartFile file) throws
-		IOException {
-		return cardService.createCard(cardDto, file);
-	}
-
-	@PatchMapping("/cards/{cardId}")
-	public ResponseEntity<CardDto> updateCard(@PathVariable Long cardId, @RequestBody CardDto cardDto) {
-		return cardService.updateCard(cardId, cardDto);
-	}
-
-	@DeleteMapping("/cards/{cardId}")
-	public ResponseEntity<String> deleteCard(@PathVariable Long cardId) {
-		return cardService.deleteCard(cardId);
-	}
-
-	@PatchMapping("/cards/{cardId}/move")
-	public ResponseEntity<String> moveCard(@PathVariable Long cardId, @RequestBody CardDto cardDto) {
-		return cardService.moveCard(cardId, cardDto);
-	}
+	// @GetMapping("/cards/{columnsId}")
+	// public List<CardDto> getCards(@PathVariable Long columnsId) {
+	// 	return cardService.getCards(columnsId);
+	// }
+	//
+	// @PostMapping("/cards")
+	// public ResponseEntity<String> createCard(@RequestBody CardDto cardDto, @RequestPart(value = "file", required = false) MultipartFile file) throws
+	// 	IOException {
+	// 	return cardService.createCard(cardDto, file);
+	// }
+	//
+	// @PatchMapping("/cards/{cardId}")
+	// public ResponseEntity<CardDto> updateCard(@PathVariable Long cardId, @RequestBody CardDto cardDto) {
+	// 	return cardService.updateCard(cardId, cardDto);
+	// }
+	//
+	// @DeleteMapping("/cards/{cardId}")
+	// public ResponseEntity<String> deleteCard(@PathVariable Long cardId) {
+	// 	return cardService.deleteCard(cardId);
+	// }
+	//
+	// @PatchMapping("/cards/{cardId}/move")
+	// public ResponseEntity<String> moveCard(@PathVariable Long cardId, @RequestBody CardDto cardDto) {
+	// 	return cardService.moveCard(cardId, cardDto);
+	// }
 }
