3b13ac1aeb682379f014856052023b7d347a1565
heroq <96118954+heroq@users.noreply.github.com>
2024-01-16 13:51:13 +0900

    Feat: 엔티티 추가 (카드, 댓글)

--- src/main/java/org/hh99/gradation/domain/entity/Comment.java
+++ src/main/java/org/hh99/gradation/domain/entity/Comment.java
@@ -1,4 +1,33 @@
 package org.hh99.gradation.domain.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "comments")
 public class Comment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "card_id")
+	private Card card;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@Column
+	private String contents;
+
+	@Column
+	private Integer parentId;
+
 }
