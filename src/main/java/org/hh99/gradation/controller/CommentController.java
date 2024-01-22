9db085139957200f5a6276ff213ebb8d26481825
heroq <96118954+heroq@users.noreply.github.com>
2024-01-22 21:36:30 +0900

    Feat: 댓글 수정 추가

--- src/main/java/org/hh99/gradation/controller/CommentController.java
+++ src/main/java/org/hh99/gradation/controller/CommentController.java
@@ -7,6 +7,7 @@ import org.hh99.gradation.service.CommentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,18 +25,23 @@ public class CommentController {
 	private final CommentService commentService;
 
 	@GetMapping("/cards/comments/{commentId}")
-	public List<CommentDto> getComments(@PathVariable Long commentId){
+	public List<CommentDto> getComments(@PathVariable Long commentId) {
 		return commentService.getComments(commentId);
 	}
 
 	@PostMapping("/cards/comments")
 	@ResponseBody
-	public ResponseEntity<CommentDto> createComments(@RequestBody CommentDto commentDto){
+	public ResponseEntity<CommentDto> createComments(@RequestBody CommentDto commentDto) {
 		return commentService.createComments(commentDto);
 	}
 
 	@DeleteMapping("/cards/comments/{commentId}")
-	public ResponseEntity deleteComments(@PathVariable Long commentId){
+	public ResponseEntity deleteComments(@PathVariable Long commentId) {
 		return commentService.deleteComments(commentId);
+	}
+
+	@PatchMapping("/cards/comments/{commentId}")
+	public ResponseEntity updateComments(@PathVariable Long commentId, @RequestBody CommentDto commentDto) {
+		return commentService.updateComments(commentId, commentDto);
 	}
 }
