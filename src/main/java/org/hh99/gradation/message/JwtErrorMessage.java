ff139b3014baba16b9aaf30aa45eefa511efd377
heroq <96118954+heroq@users.noreply.github.com>
2024-01-25 12:17:12 +0900

    Refactor: JWT 메시지 수정

--- src/main/java/org/hh99/gradation/message/JwtErrorMessage.java
+++ src/main/java/org/hh99/gradation/message/JwtErrorMessage.java
@@ -8,9 +8,9 @@ public enum JwtErrorMessage {
 	PASSWORD_VALIDATION_ERROR_MESSAGE("비밀번호는 최소 8자 이상, 15자 이하이며 알파벳 대소문자, 숫자, 특수문자로 구성되어야 합니다."),
 	AUTH_EXCEPTION_MESSAGE("권한이 없습니다."),
 	INVALID_JWT_ERROR_MESSAGE("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다."),
-	EXPIRED_JWT_ERROR_MESSAGE("Expired JWT token, 만료된 JWT token 입니다."),
+	EXPIRED_JWT_ERROR_MESSAGE("로그인을 다시 해주세요."),
 	UNSUPPORTED_JWT_ERROR_MESSAGE("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다."),
-	NULL_JWT_ERROR_MESSAGE("JWT가 없습니다. 로그인을 해주세요."),
+	NULL_JWT_ERROR_MESSAGE("로그인을 다시 해주세요."),
 	EMPTY_JWT_ERROR_MESSAGE("JWT claims is empty, 잘못된 JWT 토큰 입니다.");
 
 	private final String JwtErrorMessage;
